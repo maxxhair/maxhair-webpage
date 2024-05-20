c12db7ab7e863936cc8fc46fbd3399d3c49ad76d
Tarun-wielabs <tarun@wielabs.com>
2024-05-20 17:39:07 +0530

    3 sections added

--- app/Components/Blogs.jsx
+++ app/Components/Blogs.jsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 import { sample1 } from "../util/images";
 import Link from "next/link";
 import { firaSans, firaSansMedium } from "../util/fonts";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 function Blogs() {
   const [selected, setSelected] = useState(0);
@@ -16,28 +16,39 @@ function Blogs() {
   const sliderRef = useRef(null);
 
   const handlePrev = useCallback(() => {
-    if (!sliderRef.current) return;
     sliderRef.current.swiper.slidePrev();
-    setSelected(sliderRef.current.activeIndex);
+    setSelected((selected) =>
+      selected - 1 === -1 ? list.length - 1 : selected - 1
+    );
   }, []);
 
   const handleNext = useCallback(() => {
-    if (!sliderRef.current) return;
     sliderRef.current.swiper.slideNext();
-    setSelected(sliderRef.current.activeIndex);
+    setSelected((selected) => (selected + 1) % list.length);
   }, []);
 
   const list = [
     {
+      body: "Lorem ipsum dolor sit amet consectetur. Etiam urna elit dictum tortor.Sagittis neque a habitant commodo sit nisl. Sit facilisis rhoncus bibendum aliquam montes magna blandit lobortis quis. Eget nam quis non at bibendum nulla nulla. rhoncus bibendum",
+      link: "",
       data: "The advice we've ever heard about Hair Extensions",
       image: sample1,
     },
-    { data: "The History of Hair", image: sample1 },
     {
+      body: "Lorem ipsum dolor sit amet consectetur. Etiam urna elit dictum tortor.Sagittis neque a habitant commodo sit nisl. Sit facilisis rhoncus bibendum aliquam montes magna blandit lobortis quis. Eget nam quis non at bibendum nulla nulla. rhoncus bibendum",
+      link: "",
+      data: "The History of Hair",
+      image: sample1,
+    },
+    {
+      body: "Lorem ipsum dolor sit amet consectetur. Etiam urna elit dictum tortor.Sagittis neque a habitant commodo sit nisl. Sit facilisis rhoncus bibendum aliquam montes magna blandit lobortis quis. Eget nam quis non at bibendum nulla nulla. rhoncus bibendum",
+      link: "",
       data: "10 Things nobody told about bring a Hair Extensions asd asd asd ad asdasd",
       image: sample1,
     },
     {
+      body: "Lorem ipsum dolor sit amet consectetur. Etiam urna elit dictum tortor.Sagittis neque a habitant commodo sit nisl. Sit facilisis rhoncus bibendum aliquam montes magna blandit lobortis quis. Eget nam quis non at bibendum nulla nulla. rhoncus bibendum",
+      link: "",
       data: "10 Things nobody told about bring a Hair Extensions asd asd asd ad asdasd",
       image: sample1,
     },
@@ -52,23 +63,24 @@ function Blogs() {
       </span>
       <div className="flex lg:flex-row flex-col justify-evenly items-center lg:gap-[70px] gap-[30px] xl:w-[70%] lg:w-[90%] w-full ">
         <div className="lg:w-[calc(50%-70px)] w-[90%] h-full flex justify-center items-center ">
-          <Image src={sample1} alt="" className="w-auto h-auto rounded-xl" />
+          <Image
+            src={list[selected].image}
+            alt=""
+            className="w-auto h-auto rounded-xl"
+          />
         </div>
         <div className="flex flex-col gap-[20px] lg:w-[calc(50%-70px)] w-[90%] justify-evenly">
           <div className="flex flex-col gap-[20px]">
             <span
-              className={`${firaSans.className} lg:title-large md:title-medium title-small`}
+              className={`${firaSans.className} lg:title-large md:title-medium title-small line-clamp-1`}
             >
-              Lorem ipsum dolor sit amet consectetur.
+              {list[selected].data}
             </span>
-            <span className="text-[#242424] lg:label-large md:label-medium label-small  w-fit ">
-              Lorem ipsum dolor sit amet consectetur. Etiam urna elit dictum
-              tortor.Sagittis neque a habitant commodo sit nisl. Sit facilisis
-              rhoncus bibendum aliquam montes magna blandit lobortis quis. Eget
-              nam quis non at bibendum nulla nulla. rhoncus bibendum
+            <span className="text-[#242424] lg:label-large md:label-medium label-small line-clamp-5 w-fit ">
+              {list[selected].body}
             </span>
             <Link
-              href=""
+              href={list[selected].link}
               className="bg-[#242424] text-[#FAFAFA] lg:label-large md:label-medium label-small uppercase w-fit py-[10px] px-[30px]"
             >
               read more
@@ -76,13 +88,13 @@ function Blogs() {
           </div>
           <div className="w-full flex justify-end gap-[10px]">
             <button
-              className={`h-[60px] w-[60px] flex justify-center items-center bg-[#F2ECE2] text-[#242424]`}
+              className={`h-[60px] w-[60px] flex justify-center items-center ${"bg-[#242424] text-[#F2ECE2] "}`}
               onClick={handlePrev}
             >
               {"<"}
             </button>
             <button
-              className={`h-[60px] w-[60px] flex justify-center items-center bg-[#242424] text-[#F2ECE2]`}
+              className={`h-[60px] w-[60px] flex justify-center items-center ${"bg-[#242424] text-[#F2ECE2] "}`}
               onClick={handleNext}
             >
               {">"}
@@ -92,17 +104,20 @@ function Blogs() {
       </div>
       <div className=" xl:w-[70%] lg:w-[90%] w-full relative  ">
         <Swiper
-          onSwiper={setSelected}
           ref={sliderRef}
           spaceBetween={10}
           slidesPerView={1}
+          slidesPerGroup={1}
+          loop
           breakpoints={{
             750: {
               slidesPerView: 2,
+              delay: 5000,
             },
 
             1440: {
               slidesPerView: 3,
+              delay: 3000,
             },
           }}
         >
@@ -115,20 +130,23 @@ function Blogs() {
                   justifyContent: "flex-start",
                   alignItems: "center",
                 }}
+                onClick={() => {
+                  setSelected(index);
+                }}
               >
                 <div
                   className={` ${
                     selected === index
-                      ? "bg-[#242424] text-[#FAFAFA]"
+                      ? "bg-[#242424] text-[#F2ECE2]"
                       : "text-[#242424] bg-white"
-                  }  lg:label-large md:label-medium label-small w-full flex gap-[20px] justify-start items-center px-[20px] py-[20px] `}
+                  } cursor-pointer lg:label-large md:label-medium label-small w-full flex gap-[20px] justify-start items-center px-[20px] py-[20px] border-2`}
                 >
                   <Image
                     src={obj.image}
                     alt="error"
                     className="object-cover h-[90px] w-[90px]"
                   />
-                  <span className="truncate">{obj.data}</span>
+                  <span className="line-clamp-2">{obj.data}</span>
                 </div>
               </SwiperSlide>
             );
---
+++ app/Components/ShopByTypes.jsx
@@ -0,0 +1,83 @@
+import Image from "next/image";
+import { firaSans } from "../util/fonts";
+import { prodimg } from "../util/images";
+import Link from "next/link";
+
+function ShopByTypes() {
+  return (
+    <div className="flex flex-col bg-[#885C46] h-[90vh] w-full justify-evenly items-center py-[20px] px-[20px]">
+      <span
+        className={`${firaSans.className} font-[700] text-[#FAFAFA] lg:headline-large md:headline-medium headline-small text-center lg:w-[60%] md:w-[80%] w-full`}
+      >
+        Shop By Types
+      </span>
+      <div className="flex min-h-[600px] h-[70%] gap-[30px] justify-center lg:w-[60%] md:w-[80%] w-full">
+        <div className=" w-[calc(50%-30px)] h-full p-[20px] relative">
+          <Image
+            src={prodimg}
+            alt=""
+            className=" absolute top-0 left-0 h-full w-full object-cover "
+          />
+          <div className="flex flex-col absolute top-[30px] left-[30px] ">
+            <span
+              className={`${firaSans.className} lg:title-large md:title-medium title-small text-black `}
+            >
+              Front Lace Wig
+            </span>
+            <Link
+              href=""
+              className="border-b-2 border-black w-fit pb-[5px] font-[600]"
+            >
+              Shop now
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col w-[calc(50%-30px)] gap-[30px] h-full ">
+          <div className="w-full h-[calc(50%)] relative p-[20px]">
+            <Image
+              src={prodimg}
+              alt=""
+              className=" absolute top-0 left-0 h-full w-full object-cover "
+            />
+            <div className="flex flex-col absolute bottom-[30px] left-[30px] ">
+              <span
+                className={`${firaSans.className} lg:title-large md:title-medium title-small text-black `}
+              >
+                Front Lace Wig
+              </span>
+              <Link
+                href=""
+                className="border-b-2 border-black w-fit pb-[5px] font-[600]"
+              >
+                Shop now
+              </Link>
+            </div>
+          </div>
+          <div className="w-full h-[calc(50%)] relative p-[20px]">
+            {" "}
+            <Image
+              src={prodimg}
+              alt=""
+              className=" absolute top-0 left-0 h-full w-full object-cover "
+            />
+            <div className="flex flex-col absolute bottom-[30px] left-[30px] ">
+              <span
+                className={`${firaSans.className} lg:title-large md:title-medium title-small text-black `}
+              >
+                Front Lace Wig
+              </span>
+              <Link
+                href=""
+                className="border-b-2 border-black w-fit pb-[5px] font-[600]"
+              >
+                Shop now
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ShopByTypes;
