e83ad3ac4e6b0ffc9415f5729fe55f4bb8153b48
Tarun-wielabs <tarun@wielabs.com>
2024-05-22 11:25:29 +0530

    changes to ShopByProducts carousel - landing page

--- app/Components/ShopByProducts.jsx
+++ app/Components/ShopByProducts.jsx
@@ -1,9 +1,9 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 import "swiper/css";
 import { firaSansMedium } from "../util/fonts";
 import React, { useCallback, useEffect, useRef, useState } from "react";
-
 import Card from "./_shopbyproducts/Card";
 import { prodimg } from "../util/images";
 
@@ -15,12 +15,10 @@ function ShopByProducts() {
   const [tempArr, setTempArr] = useState([]);
   const [list, setListData] = useState(null);
 
-  const sliderRef = useRef(null);
-  const handlePrev = useCallback(() => {
-    sliderRef.current.swiper.slidePrev();
-  }, []);
-  const handleNext = useCallback(() => {
-    sliderRef.current.swiper.slideNext();
+  const sliderRefSbp = useRef(null);
+  const handleSlideChange = useCallback(() => {
+    const swiper = sliderRefSbp.current.swiper;
+    setSelected(swiper.realIndex);
   }, []);
 
   const updateFunction = () => {
@@ -94,9 +92,12 @@ function ShopByProducts() {
           spaceBetween={40}
           slidesPerView={1}
           slidesPerGroup={1}
-          onActiveIndexChange={(swiper) => {
-            setSelected(swiper.activeIndex);
+          modules={[Navigation]}
+          navigation={{
+            nextEl: ".swiper-next-button-sbp",
+            prevEl: ".swiper-prev-button-sbp",
           }}
+          onSlideChange={handleSlideChange}
         >
           {Array.from({ length: Math.ceil(list.length / numberOfCards) }).map(
             (_, index) => {
@@ -130,23 +131,21 @@ function ShopByProducts() {
         <div className="w-full flex justify-center">
           <div className="flex justify-end gap-[10px] mt-[20px] xl:w-[calc(330px*4)] lg:w-[calc(330px*3)] md:w-[calc(330px*2)] w-[calc(330px*1)]">
             <button
-              className={`h-[60px] w-[60px] flex justify-center items-center ${
+              className={`swiper-prev-button-sbp h-[60px] w-[60px] flex justify-center items-center ${
                 selected === 0
                   ? "bg-[#F2ECE2] text-[#242424] "
                   : "bg-[#242424] text-[#F2ECE2] "
               }`}
-              onClick={handlePrev}
               disabled={selected === 0}
             >
               {"<"}
             </button>
             <button
-              className={`h-[60px] w-[60px] flex justify-center items-center ${
+              className={`swiper-next-button-sbp h-[60px] w-[60px] flex justify-center items-center ${
                 selected === Math.ceil(list.length / numberOfCards) - 1
                   ? "bg-[#F2ECE2] text-[#242424] "
                   : "bg-[#242424] text-[#F2ECE2] "
               }`}
-              onClick={handleNext}
               disabled={selected === Math.ceil(list.length / numberOfCards) - 1}
             >
               {">"}
