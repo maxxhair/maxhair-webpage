cf0de27a0a6c0e742bf97f22d4168cd8d43b0efe
laxmanballa <laxman@wielabs.com>
2024-06-26 18:46:05 +0530

    feat: Add Review

---
+++ app/Components/ScrolltoTop.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+const ScrolltoTop = () => {
+  const path = usePathname();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [path]);
+  return null;
+};
+
+export default ScrolltoTop;
--- app/Components/StockCard.tsx
+++ app/Components/StockCard.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import Rating from "./Rating";
 import { firaSans } from "../util/fonts";
+import StarRating from "./StarRating";
 
 const StockCard = ({ image, name, stock }) => {
   return (
     <div className="w-full h-32 flex border-b-2 ">
-      <Image src={image} alt="product-image-error" width={80} height={140} />
+      <Image src={image} alt="product-image-error" width={80} height={100} />
       <div className="flex lg:flex-row flex-col w-full justify-between m-4 ">
         <div
           className="flex flex-row lg:flex-col w-full gap-3 lg:items-start items-center 
@@ -16,8 +16,8 @@ const StockCard = ({ image, name, stock }) => {
           </p>
           <p>Only {stock} left</p>
         </div>
-        <div className="flex">
-          <Rating count={5} value={4.5} className="mt-auto" />
+        <div className="flex items-end justify-center h-full gap-4">
+          <StarRating ratingValue={4} readOnly={true} />
           <p className=" mt-auto ml-3">15185 Reviews</p>
         </div>
       </div>
