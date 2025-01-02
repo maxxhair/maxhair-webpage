047b1deab8dfc0b2023a408ee8fc6c7e2b2cec27
Tarun Jakkula <tarun@wielabs.com>
2025-01-02 18:40:35 +0530

    changes: faq and blogs

--- app/util/axiosInstance.ts
+++ app/util/axiosInstance.ts
@@ -3,12 +3,13 @@ import { store } from "../store";
 import { LoggedUser } from "../types";
 
 export const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const baseImageUrl = process.env.NEXT_PUBLIC_IMAGE_URL;
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // Set your API base URL in environment variables
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 //Interceptor for api autorization access using access-token
